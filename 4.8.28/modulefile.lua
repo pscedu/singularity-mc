a8db8fe2197b43f73838027074ff3605cd01ed2d
Ivan Cao-Berg <icaoberg@psc.edu>
2022-05-03 15:03:17 -0400

    Added version 4.8.28

---
+++ 4.8.28/modulefile.lua
@@ -0,0 +1,38 @@
+--
+-- mc 4.8.28 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Other"
+-- "Description: Midnight Commander (also known as mc) is a free cross-platform orthodox file manager and a clone of Norton Commander."
+
+whatis("Name: mc")
+whatis("Version: 4.8.28")
+whatis("Category: Other")
+whatis("Description: Midnight Commander (also known as mc) is a free cross-platform orthodox file manager and a clone of Norton Commander.")
+
+help([[
+GNU Midnight Commander is a visual file manager, licensed under GNU General Public License and therefore qualifies as Free Software. It's a feature rich full-screen text mode application that allows you to copy, move and delete files and whole directory trees, search for files and run commands in the subshell. Internal viewer and editor are included.
+
+To load the module, type
+
+> module load mc/4.8.28
+
+To unload the module, type
+
+> module unload mc/4.8.28
+
+Documentation
+-------------
+For help, type
+
+> mc --help
+
+Tools included in this module are
+
+* mc
+]])
+
+local package = "mc"
+local version = "4.8.28"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
---
+++ 4.8.28/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+tree .
+
+singularity exec singularity-mc-4.8.28.sif mc --help
